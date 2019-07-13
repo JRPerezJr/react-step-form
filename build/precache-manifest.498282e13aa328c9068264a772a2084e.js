566404daa150993ad6103a15353c71d994004ad3
Juan P <pront23@gmail.com>
2019-07-13 17:10:44 +0900

    new version added

---
+++ build/precache-manifest.498282e13aa328c9068264a772a2084e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "cfb9e1ac95e2d8ac53b4",
+    "url": "/static/js/main.d9dc879a.chunk.js"
+  },
+  {
+    "revision": "5f2c716ee1ea0ab3e703",
+    "url": "/static/js/2.cb00a1b6.chunk.js"
+  },
+  {
+    "revision": "cfb9e1ac95e2d8ac53b4",
+    "url": "/static/css/main.85ee58c2.chunk.css"
+  },
+  {
+    "revision": "5143a308d63b69d8a9698c6a87404bb5",
+    "url": "/index.html"
+  }
+];
