e4ec1ccbb39a7268eade6482f551c8aee12bcf54
Juan <pront23@gmail.com>
2021-01-27 15:24:26 +0900

    Added default case

--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -5,69 +5,71 @@ import Confirm from './Confirm';
 import Success from './Success';
 
 export default class UserForm extends Component {
-    state = {
-        step: 1,
-        firstName: '',
-        lastName: '',
-        email: '',
-        occupation: '',
-        city: '',
-        bio: '',
-    }
-    //<--Start Methods -->
-    
-    //Proceed to next step
-    nextStep = () => {
-        const { step } = this.state;
-        this.setState({
-            step: step + 1
-        });
-    }
-    //Go back to previous step
-    previousStep = () => {
-        const { step } = this.state;
-        this.setState({
-            step: step - 1
-        });
-    }
-    //Handle field changes
-    handleChange = input => e => {
-        this.setState({[input]: e.target.value});
-    }
+  state = {
+    step: 1,
+    firstName: '',
+    lastName: '',
+    email: '',
+    occupation: '',
+    city: '',
+    bio: '',
+  };
+  //<--Start Methods -->
+
+  //Proceed to next step
+  nextStep = () => {
+    const { step } = this.state;
+    this.setState({
+      step: step + 1,
+    });
+  };
+  //Go back to previous step
+  previousStep = () => {
+    const { step } = this.state;
+    this.setState({
+      step: step - 1,
+    });
+  };
+  //Handle field changes
+  handleChange = (input) => (e) => {
+    this.setState({ [input]: e.target.value });
+  };
   render() {
     const { step } = this.state;
     const { firstName, lastName, email, occupation, city, bio } = this.state;
-    const values = { firstName, lastName, email, occupation, city, bio }
+    const values = { firstName, lastName, email, occupation, city, bio };
 
-    switch(step) {
-        case 1:
-            return (
-                <FormUserDetails
-                    nextStep={this.nextStep}
-                    handleChange={this.handleChange}
-                    values={values}
-                />
-            )
-        case 2:
-            return (
-                <FormPersonalDetails
-                    nextStep={this.nextStep}
-                    previousStep={this.previousStep}
-                    handleChange={this.handleChange}
-                    values={values}
-                />
-            )
-            
-        case 3:
-            return (
-                <Confirm
-                    nextStep={this.nextStep}
-                    previousStep={this.previousStep}
-                    values={values}
-                />
-            )
-        case 4:
-            return <Success/>;
+    switch (step) {
+      case 1:
+        return (
+          <FormUserDetails
+            nextStep={this.nextStep}
+            handleChange={this.handleChange}
+            values={values}
+          />
+        );
+      case 2:
+        return (
+          <FormPersonalDetails
+            nextStep={this.nextStep}
+            previousStep={this.previousStep}
+            handleChange={this.handleChange}
+            values={values}
+          />
+        );
+
+      case 3:
+        return (
+          <Confirm
+            nextStep={this.nextStep}
+            previousStep={this.previousStep}
+            values={values}
+          />
+        );
+      case 4:
+        return <Success />;
+      default:
+      //   do nothing
     }
   }
 }
